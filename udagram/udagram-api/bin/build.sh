febcef9479741e112b4d26fc86ce41545915de65
MohammaddAli <100025240+MohammaddAli@users.noreply.github.com>
2022-08-09 23:55:13 +0200

    init

---
+++ udagram/udagram-api/bin/build.sh
@@ -0,0 +1,12 @@
+npm install . && npm run clean && tsc && cp -rf src/config www/config && cp .npmrc www/.npmrc && cp package.json www/package.json && cd www && zip -r Archive.zip . && cd ..
+m
+x
+x
+x
+x
+x
+x
+x
+x
+x
+x
