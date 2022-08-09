1f75b6746d6a6ef04f97247d21e0cc0a3e33566d
MohammaddAli <100025240+MohammaddAli@users.noreply.github.com>
2022-08-10 00:12:01 +0200

    Update build.sh

--- udagram/udagram-api/bin/build.sh
+++ udagram/udagram-api/bin/build.sh
@@ -1,12 +1,3 @@
 npm install . && npm run clean && tsc && cp -rf src/config www/config && cp .npmrc www/.npmrc && cp package.json www/package.json && cd www && zip -r Archive.zip . && cd ..
 m
 x
-x
-x
-x
-x
-x
-x
-x
-x
-x
