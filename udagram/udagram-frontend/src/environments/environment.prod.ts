77dc36d7a9dd3fae8305ffe3153d8d72c1cff78b
MohammaddAli <100025240+MohammaddAli@users.noreply.github.com>
2022-08-21 07:36:33 +0200

    update

--- udagram/udagram-frontend/src/environments/environment.prod.ts
+++ udagram/udagram-frontend/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: true,
   appName: 'Udagram',
-  apiHost: 'http://udagram-api.eba-qidaviwh.us-east-1.elasticbeanstalk.com/api/v0'
+  apiHost: 'udagram-api.eba-cu8baymm.us-east-1.elasticbeanstalk.com/api/v0'
 };
 
 /*
--- udagram/udagram-frontend/src/environments/environment.ts
+++ udagram/udagram-frontend/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://udagram-api.eba-qidaviwh.us-east-1.elasticbeanstalk.com/api/v0'
+  apiHost: 'udagram-api.eba-cu8baymm.us-east-1.elasticbeanstalk.com/api/v0'
 };
 
 /*
