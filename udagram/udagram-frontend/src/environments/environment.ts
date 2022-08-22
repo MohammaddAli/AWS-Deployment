d82625d5fa02278a60fce36118dfbd8fab7a1b7d
MohammaddAli <100025240+MohammaddAli@users.noreply.github.com>
2022-08-22 02:20:10 +0200

    "initial commit"

--- udagram/udagram-frontend/src/environments/environment.ts
+++ udagram/udagram-frontend/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'udagram-api.eba-cu8baymm.us-east-1.elasticbeanstalk.com/api/v0'
+  apiHost: 'http://udagram-api.eba-cu8baymm.us-east-1.elasticbeanstalk.com/api/v0'
 };
 
 /*
